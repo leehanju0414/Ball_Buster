f084d077d2e257760bd41fb5c09374bf8569673c
leehanju <112833500+leehanju0414@users.noreply.github.com>
2026-05-07 19:34:27 +0900

    캐릭터 객체 추가와 입력 추가

---
+++ app/src/main/java/com/example/ball_buster/objects/player.kt
@@ -0,0 +1,40 @@
+package com.example.ball_buster.objects
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import kr.ac.tukorea.ge.spgp2026.a2dg.objects.IGameObject
+import kr.ac.tukorea.ge.spgp2026.a2dg.objects.JoyStick
+import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+import kotlin.math.cos
+
+class Player(val gctx: GameContext, val joystick: JoyStick) : IGameObject {
+    var x = 800f
+    var y = 800f
+    val radius = 50f
+    val maxSpeed = 800f
+
+    private val paint = Paint().apply { color = Color.BLUE }
+    private val rect = RectF()
+
+    fun fire() {
+        // 나중에 작살로직
+        println("작살 발사")
+    }
+
+    override fun update(gctx: GameContext) {
+        val dx = maxSpeed * joystick.power * cos(joystick.angle)
+
+        x += dx * gctx.frameTime
+
+        if (x < radius) x = radius
+        if (x > 1600f - radius) x = 1600f - radius
+
+        rect.set(x - radius, y - radius, x + radius, y + radius)
+    }
+
+    override fun draw(canvas: Canvas) {
+        canvas.drawRect(rect, paint)
+    }
+}
--- app/src/main/java/com/example/ball_buster/scene/MainScene.kt
+++ app/src/main/java/com/example/ball_buster/scene/MainScene.kt
@@ -1,12 +1,38 @@
 package com.example.ball_buster.scene
 
+import android.view.MotionEvent
+import com.example.ball_buster.objects.Player
+import kr.ac.tukorea.ge.spgp2026.a2dg.objects.JoyStick
 import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
 import kr.ac.tukorea.ge.spgp2026.a2dg.scene.World
 import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+import com.example.ball_buster.R
 
 class MainScene(gctx: GameContext) : Scene(gctx) {
-    override val world = World(MainLayer.entries.toTypedArray())
+    val joystick = JoyStick(gctx, R.drawable.joy_bg, R.drawable.joy_thumb, 300f, 700f, 150f, 50f)
 
-    init {
+    val player = Player(gctx, joystick)
+
+    override val world = World(MainLayer.entries.toTypedArray()).apply {
+        add(player, MainLayer.PLAYER)
+        add(joystick, MainLayer.UI)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val pt = gctx.metrics.fromScreen(event.x, event.y)
+        val isRightSide = pt.x > gctx.metrics.width / 2
+
+        val isActionDown = event.actionMasked == MotionEvent.ACTION_DOWN ||
+                event.actionMasked == MotionEvent.ACTION_POINTER_DOWN
+
+        if (isRightSide) {
+            if (isActionDown) {
+                player.fire()
+            }
+        } else {
+            joystick.onTouchEvent(event)
+        }
+
+        return true
     }
 }
